40e8f1ed0269f90e051a0742cb459413073df0f2
Aaron Ramirez <aramxc@gmail.com>
2017-09-27 16:18:25 -0600

    added contact form modal

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ window.onload = function hello() {
 
   });
 };
+
+// Get contact overlay
+var contactModal = getElementById('contact-overlay');
+
+//Get contact overlay link
+var contactLink = getElementById('overlayLink');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the link, open the modal
+function openContactForm() {
+  contactModal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  contactModal.style.display = 'none';
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+      contactModal.style.display = 'none';
+    }
+  };
