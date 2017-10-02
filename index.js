23971cf7d67561224b30b4c416e41b6934845f30
Aaron Ramirez <aramxc@gmail.com>
2017-10-02 10:56:17 -0600

    removed page modals, decided to create new page element for each. updated CSS and JS accordingly. Also added blue-theme and tan-theme.

--- index.js
+++ index.js
@@ -16,52 +16,26 @@ window.onload = function hello() {
 
 //get modal elements
 var contactModal = document.getElementById('contact-modal');
-var projectsModal = document.getElementById('projects-modal');
-var aboutModal = document.getElementById('about-modal');
 
 //get modal Buttons
 var contactModalBtn = document.getElementById('contactModalBtn');
-var projectsModalBtn = document.getElementById('projectsModalBtn');
-var aboutModalBtn = document.getElementById('aboutModalBtn');
 
 //get close Buttons
 var contactClose = document.getElementById('contactClose');
-var projectsClose = document.getElementById('projectsClose');
-var aboutClose = document.getElementById('aboutClose');
 
 //functions to open modals
 contactModalBtn.onclick = function openContactModal() {
   contactModal.style.display = 'block';
 };
 
-projectsModalBtn.onclick = function openProjectsModal() {
-  projectsModal.style.display = 'block';
-};
-
-aboutModalBtn.onclick = function openAboutModal() {
-  aboutModal.style.display = 'block';
-};
-
-//functions to close modal
+//functions to close modals
 contactClose.onclick = function closeContactModal() {
   contactModal.style.display = 'none';
-};
-
-projectsClose.onclick = function closeProjectsModal() {
-  projectsModal.style.display = 'none';
-};
-
-aboutClose.onclick = function closeAboutModal() {
-  aboutModal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == contactModal) {
     contactModal.style.display = 'none';
-  } else if (event.target == projectsModal) {
-    projectsModal.style.display = 'none';
-  } else if (event.target == aboutModal) {
-    aboutModal.style.display = 'none';
   }
 };
