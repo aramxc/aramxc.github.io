facb76f2477ee80c6838d763023be60bdd73b525
Aaron Ramirez <aramxc@gmail.com>
2017-10-12 10:32:13 -0600

    updated styling, added theme and relevant javascript plugins

---
+++ Resources/Scripts/main.js
@@ -0,0 +1,11 @@
+window.onload = function hello() {
+
+  //typed.js script
+  var typed = new Typed('#hello', {
+    strings: ['', 'Hello, ^100 I\'m Aaron.'],
+    typeSpeed: 60,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 200,
+  });
+};
